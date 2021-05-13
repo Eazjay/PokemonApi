6d8929af1e529bc3d4035687c31cc3119a75abba
MyMac <mymac@Joels-MacBook-Pro.local>
2021-05-13 17:06:08 -0500

    initial commit

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -10,12 +10,14 @@ const Pokemon = () => {
 
         return (
             <div className="container mt-3">
-                <button onClick={fetchPokemon} className="btn btn-primary ">Fetch Pokemon</button>
-                <div className="mt-5">
+                <div className="text-center">
+                    <button onClick={fetchPokemon} className="btn btn-primary mt-5">Fetch Pokemon</button>
+                </div>
+                <div className="w-25 mx-auto mt-5">
                     {pokemon.length > 0 && pokemon.map((pokemon, index) =>{
                         return (
                             <div key={index}>
-                                <h6>{pokemon.name}</h6>
+                                <h6 className="text-center">{pokemon.name}</h6>
                             </div>
                         )
                     })}
